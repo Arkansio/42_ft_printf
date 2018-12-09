29db5a451c613c1ff7795566d741a277338256c6
Arkansio <38956983+Arkansio@users.noreply.github.com>
2018-12-09 03:34:17 +0100

    mise en place

--- includes/printf.h
+++ includes/printf.h
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/08 21:19:48 by mgessa            #+#    #+#             */
-/*   Updated: 2018/12/08 21:23:11 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/12/09 02:37:44 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 # define PRINTF_H
 
 #include "libft.h"
+
+typedef struct		s_args
+{
+	int		flags;
+}					t_args;
 
 int		ft_printf(const char *format, ...);
 
