2440441ec1cd95d254d5c86467f2de7c80798c88
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-12-17 01:44:10 +0100

    modulo handle min width

--- src/_p_modulo.c
+++ src/_p_modulo.c
@@ -6,17 +6,24 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 23:54:06 by mgessa            #+#    #+#             */
-/*   Updated: 2018/12/17 00:23:42 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/12/17 01:39:56 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "struct.h"
 
+void        ft_write_multiple(int sz, char c)
+{
+    while (sz-- > 0)
+        write(1, &c, 1);
+}
+
 char        *_p_modulo(t_proper *properties, va_list *args)
 {
-    (void)properties;
     (void)args;
 
+	printf("Properties: %d", properties->min_w);
+	ft_write_multiple(properties->min_w - 1, ' ');
     ft_putfaststr("%%", 1);
     return (NULL);
 }
