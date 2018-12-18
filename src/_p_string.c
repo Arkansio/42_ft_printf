45b68f91c54652bb60ec4e325925a3226acca3e6
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-12-18 18:53:55 +0100

    corectly print string

--- src/_p_string.c
+++ src/_p_string.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 22:33:20 by mgessa            #+#    #+#             */
-/*   Updated: 2018/12/18 01:44:33 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/12/18 18:53:45 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,8 @@ int			_p_string(t_proper *properties, va_list *args)
 
 	str = va_arg(*args, char *);
 	chain_z = properties->precision;
-	if (properties->precision == -1)
-		chain_z = ft_strlen(str);
-
+	if (properties->precision == -1 || properties->precision >= chain_z)
+		chain_z = (int)ft_strlen(str);
  //   printf("o-o-o-o-o _p_string o-o-o-o-o\n\n");
 	if (properties->min_w > chain_z)
 		ft_write_multiple(properties->min_w - chain_z, ' ');
