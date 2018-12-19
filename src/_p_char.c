abfdb2b88188a421e414336b2f93408b91b777a9
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-12-19 03:55:40 +0100

    add padding

--- src/_p_char.c
+++ src/_p_char.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 21:22:12 by mgessa            #+#    #+#             */
-/*   Updated: 2018/12/17 23:06:42 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/12/19 03:53:13 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 int			_p_char(t_proper *properties, va_list *args)
 {
     unsigned char   c;
+	int				chain_z;
 
 	c = (unsigned char)va_arg(*args, int);
-
+	chain_z = 1;
  //   printf("o-o-o-o-o _p_string o-o-o-o-o\n\n");
-	if (properties->min_w > 1)
-		ft_write_multiple(properties->min_w - 1, ' ');
+	print_first_padding(properties, &chain_z);
+	if (properties->precision < 1)
+		chain_z = 1;
     write(1, &c, 1);
-	if (properties->min_w > 1)
-    	return (properties->min_w);
-	else
-		return (1);
+	print_end_padding(properties, &chain_z);
+	return (chain_z);
 }
