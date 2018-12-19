5eded19359cdce6fed4635b6c0e057d92546cb3d
arkansio <38956983+Arkansio@users.noreply.github.com>
2018-12-19 04:07:19 +0100

    set string limit to ft_putfaststr.c

--- src/ft_putfaststr.c
+++ src/ft_putfaststr.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 22:40:54 by mgessa            #+#    #+#             */
-/*   Updated: 2018/12/17 02:29:27 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/12/19 04:05:26 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ void        ft_putfaststr(const char *str, int max)
 {
 	int     i;
 
-	i = ft_strlen(str);
+	i = (int)ft_strlen(str);
 	if (max != -1)
-		i = max;
+	{
+		if (max < i)
+			i = max;
+	}
 	if (i >= 1)
 		write(1, str, i);
 }
