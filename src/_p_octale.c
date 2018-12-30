bc477ffd76802ba773677e8f35b240e4990ac39b
Arkansio <38956983+Arkansio@users.noreply.github.com>
2018-12-30 18:17:46 +0100

    return value in octale

--- src/_p_octale.c
+++ src/_p_octale.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 16:29:25 by mgessa            #+#    #+#             */
-/*   Updated: 2018/12/30 14:03:23 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/12/30 18:17:33 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ static int      calcul_blank_w(t_proper *properties, int str_sz, long long val)
         sz = properties->precision;
     if (sz < properties->min_w)
         return (properties->min_w - sz);
+
     return (0);
 }
 
@@ -33,10 +34,14 @@ int				_p_octale(t_proper *properties, va_list *args)
 	char	*str;
 	int		val;
 	int		sz_result;
+	int		total_sz;
 
+	total_sz = 0;
 	val = va_arg(*args, int);
 	str = ft_itoa_base(val, 8);
 	sz_result = (int)ft_strlen(str);
+	if (properties->precision == -1)
+		properties->precision = 1;
 	if (!contain_flag(properties, minus))
 		ft_write_multiple(calcul_blank_w(properties, sz_result, val), contain_flag(properties, zero) ? '0' : ' ');
 	if (properties->precision > sz_result)
@@ -44,5 +49,6 @@ int				_p_octale(t_proper *properties, va_list *args)
 	ft_putfaststr(str, -1);
 	if (contain_flag(properties, minus))
 		ft_write_multiple(calcul_blank_w(properties, sz_result, val), ' ');
-	return (1);
+	total_sz += calcul_blank_w(properties, sz_result, val) + sz_result + (properties->precision - sz_result);
+	return (total_sz);
 }
--- src/ft_itoa_base.c
+++ src/ft_itoa_base.c
@@ -6,25 +6,11 @@
 /*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/28 23:38:34 by mgessa            #+#    #+#             */
-/*   Updated: 2018/12/30 17:03:55 by mgessa           ###   ########.fr       */
+/*   Updated: 2018/12/30 18:03:04 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "struct.h"
-
-static char		*ft_get_min(char *dest)
-{
-	int i;
-	char tab[] = "-2147483648";
-	i = 0;
-	while (tab[i])
-	{
-		dest[i] = tab[i];
-		i++;
-	}
-	dest[i] = '\0';
-    return (dest);
-}
 
 static int		ft_zero(char *str, int val)
 {
@@ -39,27 +25,12 @@ char	*ft_itoa_base(int value, int base)
 {
 	char *val;
 	int i;
-	int is_neg;
 
-	is_neg = 0;
 	i = 0;
 	if (!(val = malloc(sizeof(char) * 30)))
 		return (NULL);
 	if (ft_zero(val, value))
 		return (val);
-    if (base < 2 || base > 16)
-	{
-		val[0] = '\0';
-		return (val);
-	}
-	if (base == 10 && value < 0)
-	{
-		if (value == -2147483648)
-            return ft_get_min(val);
-		value *= -1;
-    }
-	else if (value < 0)
-		value *= -1;
 	while (value != 0)
 	{
 		if (value % base >= 10)
@@ -68,8 +39,6 @@ char	*ft_itoa_base(int value, int base)
 			val[i++] = '0' + value % base;
 		value /= base;
 	}
-	if (base == 10 && is_neg == 1)
-		val[i++] = '-';
 	val[i] = '\0';
 	ft_strrev(val);
 	return (val);
