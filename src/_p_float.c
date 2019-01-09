de361672914b849f97c77cc82aa09ae1e3dbff29
arkansio <38956983+Arkansio@users.noreply.github.com>
2019-01-09 03:43:04 +0100

    add float

--- src/_p_float.c
+++ src/_p_float.c
@@ -6,16 +6,32 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 23:36:25 by mgessa            #+#    #+#             */
-/*   Updated: 2018/12/17 02:49:53 by mgessa           ###   ########.fr       */
+/*   Updated: 2019/01/09 03:36:21 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "struct.h"
 
+static void     print_decimal(double db)
+{
+    db -= (double)(int)db;
+	while (db != 0.0)
+	{
+		db *= 10;
+		ft_putchar('0' + (int)db);
+		db -= (double)(int)db;
+		
+    }
+}
+
 int             _p_float(t_proper *properties, va_list *args)
 {
+    double  db;
+
+    db = va_arg(*args, double);
+    ft_putnbr((int)db);
+    ft_putchar('.');
+    print_decimal(db);
     (void)properties;
-//    printf("_p_float\n");
-    printf("%f", va_arg(*args, double));
     return (0);
 }
