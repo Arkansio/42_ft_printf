c7337b2320897ded2cc7121776c4eca05ba995f2
arkansio <38956983+Arkansio@users.noreply.github.com>
2019-01-14 01:01:52 +0100

    fill with zero

--- src/ft_convert.c
+++ src/ft_convert.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_convert.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgessa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 18:43:50 by mgessa            #+#    #+#             */
-/*   Updated: 2018/12/30 18:57:26 by mgessa           ###   ########.fr       */
+/*   Updated: 2019/01/14 01:01:07 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int			ft_convert(const char *str, int max, va_list *args)
 	t_proper    properties;
 	int			i_tab;
 	char		*new_str;
+	int			copy_l;
 
 	new_str = NULL;
 	ft_parse_properties(&properties, str, max);
@@ -25,9 +26,14 @@ int			ft_convert(const char *str, int max, va_list *args)
 //	printf("\nPrecision: %d\n", properties.precision);
 //	printf("Min_width: %d\n\n", properties.min_w);
 	if (max == 1)
+	{
 		return (0);
+	}
 	if (i_tab != -1)
-		return (g_types[i_tab].func(&properties, args));
-	ft_write_multiple(properties.min_w - 1, ' ');
+	{
+		copy_l = g_types[i_tab].func(&properties, args);
+		return (copy_l);
+	}
+	ft_write_multiple(properties.min_w - 1, contain_flag(&properties, zero) ? '0' : ' ');
 	return (properties.min_w >= 0 ? properties.min_w - 1 : 0);
 }
