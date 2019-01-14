8e6a2235968c7f5a038dd3507fd60abd69d2440b
arkansio <38956983+Arkansio@users.noreply.github.com>
2019-01-14 01:21:30 +0100

    pass to static

--- src/ft_convert.c
+++ src/ft_convert.c
@@ -6,19 +6,19 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 18:43:50 by mgessa            #+#    #+#             */
-/*   Updated: 2019/01/14 01:18:27 by mgessa           ###   ########.fr       */
+/*   Updated: 2019/01/14 01:21:21 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "globales.h"
 
-void		delete_flag(void *content, size_t sz)
+static void		delete_flag(void *content, size_t sz)
 {
 	(void)sz;
 	free(content);
 }
 
-int			ft_convert(const char *str, int max, va_list *args)
+int				ft_convert(const char *str, int max, va_list *args)
 {
 	t_proper    properties;
 	int			i_tab;
