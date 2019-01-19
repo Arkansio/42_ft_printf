273b3a1d65748988b92daf2c7ff249ce45b0865f
arkansio <38956983+Arkansio@users.noreply.github.com>
2019-01-19 04:31:25 +0100

    norme octale

--- src/p_octale.c
+++ src/p_octale.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 16:29:25 by mgessa            #+#    #+#             */
-/*   Updated: 2019/01/15 20:56:25 by mgessa           ###   ########.fr       */
+/*   Updated: 2019/01/19 04:30:54 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,23 @@ static int		calcul_blank_w(t_proper *properties, int str_sz, long long val)
 	return (0);
 }
 
+static void		print(t_proper *properties, int sz_result,
+long long val, char *str)
+{
+	if (!contain_flag(properties, minus))
+		ft_write_multiple(calcul_blank_w(properties, sz_result, val),
+		contain_flag(properties, zero) ? '0' : ' ');
+	if (properties->precision > sz_result)
+		ft_write_multiple(properties->precision - sz_result, '0');
+	if (contain_flag(properties, diez) && ((properties->precision
+	<= sz_result && val != 0) || (properties->precision == 0 && val == 0)))
+		ft_putchar('0');
+	if (val != 0 || properties->precision != 0)
+		ft_putfaststr(str, -1);
+	if (contain_flag(properties, minus))
+		ft_write_multiple(calcul_blank_w(properties, sz_result, val), ' ');
+}
+
 int				p_octale(t_proper *properties, va_list *args)
 {
 	char		*str;
@@ -38,24 +55,17 @@ int				p_octale(t_proper *properties, va_list *args)
 	total_sz = 0;
 	can_ignore_zero(properties);
 	val = get_uint_flags(properties, args);
-	str = ft_itoa_base(val, 8, 0);
+	if (!(str = ft_itoa_base(val, 8, 0)))
+		return (-1);
 	sz_result = (int)ft_strlen(str);
 	if (properties->precision == -1)
 		properties->precision = 1;
 	if (properties->precision == 0 && val == 0)
 		sz_result--;
-	if (contain_flag(properties, diez) && ((properties->precision <= sz_result && val != 0) || (properties->precision == 0 && val == 0)))
+	if (contain_flag(properties, diez) && ((properties->precision <= sz_result
+	&& val != 0) || (properties->precision == 0 && val == 0)))
 		sz_result++;
-	if (!contain_flag(properties, minus))
-		ft_write_multiple(calcul_blank_w(properties, sz_result, val), contain_flag(properties, zero) ? '0' : ' ');
-	if (properties->precision > sz_result)
-		ft_write_multiple(properties->precision - sz_result, '0');
-	if (contain_flag(properties, diez) && ((properties->precision <= sz_result && val != 0) || (properties->precision == 0 && val == 0)))
-		ft_putchar('0');
-	if (val != 0 || properties->precision != 0)
-		ft_putfaststr(str, -1);
-	if (contain_flag(properties, minus))
-		ft_write_multiple(calcul_blank_w(properties, sz_result, val), ' ');
+	print(properties, sz_result, val, str);
 	total_sz += calcul_blank_w(properties, sz_result, val) + sz_result;
 	if (properties->precision > sz_result)
 		total_sz += (properties->precision - sz_result);
