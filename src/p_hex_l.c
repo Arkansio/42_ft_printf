c7fa1d71da857eb4b6cf64b2643b17b466df4491
arkansio <38956983+Arkansio@users.noreply.github.com>
2019-01-19 05:23:04 +0100

    norme

--- src/p_hex_l.c
+++ src/p_hex_l.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 19:21:25 by mgessa            #+#    #+#             */
-/*   Updated: 2019/01/14 22:48:46 by mgessa           ###   ########.fr       */
+/*   Updated: 2019/01/19 05:22:28 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,21 @@ static int		calcul_blank_w(t_proper *properties, int str_sz, long long val)
 	return (0);
 }
 
-static void		print_all(t_proper *properties, char *str, long long val, int sz_result)
+static void		print_all(t_proper *properties, char *str,
+long long val, int sz_result)
 {
-	if (contain_flag(properties, diez) && ((properties->precision <= sz_result && val != 0) && !(properties->precision == 0 && val == 0)) && contain_flag(properties, zero))
+	if (contain_flag(properties, diez) &&
+	((properties->precision <= sz_result && val != 0) &&
+	!(properties->precision == 0 && val == 0)) &&
+	contain_flag(properties, zero))
 		ft_putfaststr("0x", -1);
 	if (!contain_flag(properties, minus))
-		ft_write_multiple(calcul_blank_w(properties, sz_result, val), contain_flag(properties, zero) ? '0' : ' ');
-	if (contain_flag(properties, diez) && ((properties->precision <= sz_result && val != 0) && !(properties->precision == 0 && val == 0)) && !contain_flag(properties, zero))
+		ft_write_multiple(calcul_blank_w(properties, sz_result, val),
+		contain_flag(properties, zero) ? '0' : ' ');
+	if (contain_flag(properties, diez) &&
+	((properties->precision <= sz_result && val != 0)
+	&& !(properties->precision == 0 && val == 0)) &&
+	!contain_flag(properties, zero))
 		ft_putfaststr("0x", -1);
 	if (properties->precision > sz_result)
 		ft_write_multiple(properties->precision - sz_result, '0');
@@ -55,13 +63,16 @@ int				p_hex_l(t_proper *properties, va_list *args)
 	total_sz = 0;
 	can_ignore_zero(properties);
 	val = get_uint_flags(properties, args);
-	str = ft_itoa_base(val, 16, 'a');
+	if (!(str = ft_itoa_base(val, 16, 'a')))
+		return (-1);
 	sz_result = (int)ft_strlen(str);
 	if (properties->precision == -1)
 		properties->precision = 1;
 	if (properties->precision == 0 && val == 0)
 		sz_result--;
-	if (contain_flag(properties, diez) && ((properties->precision <= sz_result && val != 0) && !(properties->precision == 0 && val == 0)))
+	if (contain_flag(properties, diez) &&
+	((properties->precision <= sz_result && val != 0)
+	&& !(properties->precision == 0 && val == 0)))
 		sz_result += 2;
 	print_all(properties, str, val, sz_result);
 	total_sz += calcul_blank_w(properties, sz_result, val) + sz_result;
