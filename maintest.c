041d63d8ff25241718421a70d044a3be1f2d7630
arkansio <38956983+Arkansio@users.noreply.github.com>
2019-01-19 04:49:21 +0100

    add tests

--- maintest.c
+++ maintest.c
@@ -6,7 +6,7 @@
 /*   By: mgessa <mgessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/08 20:58:09 by mgessa            #+#    #+#             */
-/*   Updated: 2019/01/17 04:56:21 by mgessa           ###   ########.fr       */
+/*   Updated: 2019/01/19 04:45:50 by mgessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -720,6 +720,17 @@ int		main()
 	char		t101[] = "[t101] Print test: %#x f %.1+010f\n";
 	nb1 = printf(t101, 10, -0.0505);
 	nb2 = ft_printf(t101, 10, -0.0505);
+	print_result(nb1, nb2);
+
+
+	char		t102[] = "[t102] Print test: %#.f %.1 010f\n";
+	nb1 = printf(t102, 0, -0.0505);
+	nb2 = ft_printf(t102, 0, -0.0505);
+	print_result(nb1, nb2);
+
+	char		t103[] = "[t103] Print test: %#.d %.1 010d\n";
+	nb1 = printf(t103, -0, -0);
+	nb2 = ft_printf(t103, -0, -0);
 	print_result(nb1, nb2);
 
 }
